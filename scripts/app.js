43b88d1d2d875a43b48e37aa34e3d928d007b842
jayant <jayantparihar@yahoo.com>
2021-04-21 13:55:14 -0700

    added function for images

--- scripts/app.js
+++ scripts/app.js
@@ -1,7 +1,15 @@
-jsx
+//jsx
 const albums = [
-  { name: "Album 1", price: 12.99, inCart: false }, 
-  { name: "Album 2", price: 14.99, inCart: false }, 
-  { name: "Album 3", price: 9.99, inCart: false }, 
-  { name: "Album 4", price: 19.99, inCart: false }
-];
+    { name: "Album1", price: 12.99, inCart: false },
+    { name: "Album2", price: 14.99, inCart: false },
+    { name: "Album3", price: 9.99, inCart: false },
+    { name: "Album4", price: 19.99, inCart: false }
+  ];
+  
+  function nextImage() {
+    for (var i = 0; i < albums.length; i++) {
+      var sr = "Album" + (i + 1);
+      var imgs = document.getElementById(sr);
+      imgs.src = "Images/" + albums[i].name + ".png";
+    }
+  }
